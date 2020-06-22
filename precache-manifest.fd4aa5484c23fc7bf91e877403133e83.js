b7d7941e3fa8406183e15b6e8add64b029940921
Shivesh <guptashiv84@gmail.com>
2020-06-22 17:39:58 +0530

    Updates

---
+++ precache-manifest.fd4aa5484c23fc7bf91e877403133e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6841c3af9d9549babd52fe85b85af1af",
+    "url": "/shivesh947/catch-of-the-day/index.html"
+  },
+  {
+    "revision": "59915f1455a2c3bbe421",
+    "url": "/shivesh947/catch-of-the-day/static/css/main.407725eb.chunk.css"
+  },
+  {
+    "revision": "452d45a222882099760a",
+    "url": "/shivesh947/catch-of-the-day/static/js/2.b78cd2d4.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/shivesh947/catch-of-the-day/static/js/2.b78cd2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59915f1455a2c3bbe421",
+    "url": "/shivesh947/catch-of-the-day/static/js/main.b51a7e3d.chunk.js"
+  },
+  {
+    "revision": "ad32a16f53cc39115ed0",
+    "url": "/shivesh947/catch-of-the-day/static/js/runtime-main.f6abd24a.js"
+  }
+]);
